4879ee9d477deb17ff343c4aa6bd5a623740ad92
Renan Santana <renan.santana@instruct.com.br>
2023-01-02 17:18:18 -0300

    Implement core feature

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
-function send_data(event) {
+function create_links(event) {
   event.preventDefault()
 
-  const url = "https://webhook.site/d900ab98-677d-4a11-9834-77127483258d"
-  const data = { content: document.getElementById("content").value }
+  const project_id = "252"
+  const url = "https://git.mop.equinix.com.br/api/v4/"
+  let content = document.getElementById("content").value
+  const regex = /[A-Z]{3,4}-\d{3}/gm;
+  let matches
 
-  fetch(url, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(data),
-    mode: "no-cors",
-  })
-    .then((response) => {
-      console.log(response)
+  while ((matches = regex.exec(content)) !== null) {
+    // This is necessary to avoid infinite loops with zero-width matches
+    if (matches.index === regex.lastIndex) {
+      regex.lastIndex++
+    }
+
+    matches.forEach((issue_code) => {
+      // Get issue URL on GitLab API
+      let query = new URLSearchParams()
+      query.append("search", issue_code)
+      const mounted_url = `${url}/projects/${project_id}/issues?${query.toString()}`
+
+      fetch(mounted_url, {
+        method: "GET",
+        headers: {"PRIVATE-TOKEN": "token"},  // TODO: change here
+      })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.length > 0) {
+          // Change issue code by markdown link pattern
+          const issue_url = data[0]["web_url"]
+          content = content.replace(issue_code, `[${issue_code}](${issue_url})`)
+          document.getElementById("content").value = content
+        }
+      })
     })
+  }
 }
 
 // Events listeners
-document.getElementById("create").addEventListener("click", send_data)
+document.getElementById("create").addEventListener("click", create_links)
